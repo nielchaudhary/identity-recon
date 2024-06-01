dc777ac231b908067fd4e1737f80ef24dc93ed25
{{nielchaudhary}} <119964539+nielchaudhary@users.noreply.github.com>
2024-06-02 02:13:12 +0530

    added dbconnect, fixes and schema

--- dist/utils/database.js
+++ dist/utils/database.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -14,14 +37,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.dbConnect = void 0;
 const promise_1 = __importDefault(require("mysql2/promise"));
+const dotenv = __importStar(require("dotenv"));
 const logger_1 = require("../utils/logger");
+dotenv.config();
 const logger = new logger_1.Logger('DBLogger');
 const dbConnect = () => __awaiter(void 0, void 0, void 0, function* () {
     const pool = promise_1.default.createPool({
         host: '127.0.0.1',
         user: 'root',
-        password: 'neepushika20',
         database: 'contacts',
+        password: process.env.DB_PASSWORD,
         connectionLimit: 10,
     });
     try {
