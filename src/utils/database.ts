f15357e9d9e2e7392693ff6a640ccb97a2e50106
{{nielchaudhary}} <119964539+nielchaudhary@users.noreply.github.com>
2024-06-03 04:23:13 +0530

    added pool is sequelize

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -9,11 +9,21 @@ dotenv.config();
 
 const sequelize = new Sequelize({
   dialect: 'mysql',
+  dialectOptions: {
+    connectTimeout: 60000
+  },
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   port: 3306,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  pool: {
+    max: 15,
+    min: 5,
+    idle: 20000,
+    evict: 15000,
+    acquire: 30000
+  },
   models: [__dirname + '/../contactModel.ts'], 
 });
 
